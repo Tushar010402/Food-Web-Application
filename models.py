17c5338b08c0a69b37354bd2f3af15ac402c9be9
Tushar Agrawal <107952347+Tushar010402@users.noreply.github.com>
2022-06-23 12:04:44 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,20 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+# Create your models here.
+class Item(models.Model):
+
+    def __str__(self):
+        return self.item_name
+    user_name = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default="")
+    
+    # delete this afterwards
+    def get_absolute_url(self):
+           return reverse("food:detail", kwargs={"pk": self.pk})
+
+# Create your models here.
